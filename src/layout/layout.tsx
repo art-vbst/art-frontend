104976e9597bb75825e68002cc29ea5936e07079
Talmage <talmage.bergeson@gmail.com>
2025-10-15 18:05:27 -0600

    clean: remove unused vars

--- src/layout/layout.tsx
+++ src/layout/layout.tsx
@@ -2,16 +2,12 @@ import { Outlet } from 'react-router-dom';
 import { Cart } from '~/components/Cart/Cart';
 import { CartBadge } from '~/components/Cart/CartBadge';
 import { Navbar } from '~/components/Navbar/Navbar';
-import { useIsSystemRoute } from '~/hooks/use-is-system-route';
 import { useScrollToTop } from '~/hooks/use-scroll-to-top';
 import { useValidateCart } from '~/hooks/use-validate-cart';
-import { cn } from '~/utils/cn';
 
 export const Layout = () => {
   useScrollToTop();
   useValidateCart();
-
-  const isSystemRoute = useIsSystemRoute();
 
   return (
     <div className="flex min-h-dvh flex-col">
